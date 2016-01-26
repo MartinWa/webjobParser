bf9c2a03ff77e3b73f37dbd4adfd423093363da8
Martin Wåger <martin.w@comaround.se>
2016-01-26 17:19:24 +0100

    start

---
+++ webjobParser/Dto/CustomEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace webjobParser.Dto
+{
+    class CustomEntry
+    {
+        public string Id { get; set; }
+        public string FunctionDisplayTitle { get; set; }
+        public EntryStatus Status { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan Duration { get; set; }
+        public object ExceptionMessage { get; set; }
+        public object ExceptionType { get; set; }
+    }
+
+    internal enum EntryStatus
+    {
+        CompletedSuccess,
+        Running,
+        CompletedFailed,
+        NeverFinished
+    }
+}
--- webjobParser/Program.cs
+++ webjobParser/Program.cs
@@ -44,47 +44,60 @@ namespace webjobParser
                     data = functionInvokes.Data;
                     entries.AddRange(data.entries);
                 }
-                const string groupName = "portalId";
-                var portalIdRegexString = $@"((?<{groupName}>\d+))";
-                var portalIdRegex = new Regex(portalIdRegexString, RegexOptions.None);
-                var logs = new List<WebJobLog>();
-                foreach (var entry in entries)
-                {
-                    var id = entry.id;
-                    var portalId = 0;
-                    var match = portalIdRegex.Match(entry.functionDisplayTitle);
-                    if (match.Success)
-                    {
-                        var portalIdString = match.Groups[groupName].Value;
-                        int.TryParse(portalIdString, out portalId);
-                    }
-                    var date = Convert.ToDateTime(entry.whenUtc);
-                    var duration = TimeSpan.FromMilliseconds(entry.duration);
-                    var logRequest = new RestRequest($"azurejobs/api/log/output/{id}?start=0", Method.GET);
-                    var log = client.Execute(logRequest);
-                    logs.Add(new WebJobLog
-                    {
-                        Id = id,
-                        PortalId = portalId,
-                        Date = date,
-                        Duration = duration,
-                        Log = log.Content
-                    });
-                    Console.WriteLine($"Log id {id} portal id {portalId} date {date} duration {duration}");
-                }
-                var json = JsonConvert.SerializeObject(logs);
+                //const string groupName = "portalId";
+                //var portalIdRegexString = $@"((?<{groupName}>\d+))";
+                //var portalIdRegex = new Regex(portalIdRegexString, RegexOptions.None);
+                //var logs = new List<WebJobLog>();
+                //foreach (var entry in entries)
+                //{
+                //    var id = entry.id;
+                //    var portalId = 0;
+                //    var match = portalIdRegex.Match(entry.functionDisplayTitle);
+                //    if (match.Success)
+                //    {
+                //        var portalIdString = match.Groups[groupName].Value;
+                //        int.TryParse(portalIdString, out portalId);
+                //    }
+                //    var date = Convert.ToDateTime(entry.whenUtc);
+                //    var duration = TimeSpan.FromMilliseconds(entry.duration);
+                //    var logRequest = new RestRequest($"azurejobs/api/log/output/{id}?start=0", Method.GET);
+                //    var log = client.Execute(logRequest);
+                //    logs.Add(new WebJobLog
+                //    {
+                //        Id = id,
+                //        PortalId = portalId,
+                //        Date = date,
+                //        Duration = duration,
+                //        Log = log.Content
+                //    });
+                //    Console.WriteLine($"Log id {id} portal id {portalId} date {date} duration {duration}");
+                //}
+                var json = JsonConvert.SerializeObject(entries);
                 File.WriteAllText(path, json);
             }
             var jsonString = File.ReadAllText(path);
-            var allLogs = JsonConvert.DeserializeObject<List<WebJobLog>>(jsonString);
-            var portalsWithWarnings = allLogs.Where(log => log.Log.Contains("Warning")).OrderBy(l => l.PortalId).ToList();
-            var storageAccount = CloudStorageAccount.Parse(Secrets.StorageAccountConnectionString);
-            var queueClient = storageAccount.CreateCloudQueueClient();
-            var queue = queueClient.GetQueueReference(Secrets.QueueName);
-            foreach (var log in portalsWithWarnings)
+            var allEntries = JsonConvert.DeserializeObject<List<Entry>>(jsonString);
+            var customEntries = allEntries.Select(a => new CustomEntry
             {
-                queue.AddMessage(new CloudQueueMessage(log.PortalId.ToString()));
-            }
+                Id = a.id,
+                FunctionDisplayTitle = a.functionDisplayTitle,
+                Date = Convert.ToDateTime(a.whenUtc),
+                Duration = TimeSpan.FromMilliseconds(a.duration),
+                Status = (EntryStatus)Enum.Parse(typeof(EntryStatus), a.status),
+            }).ToList();
+            var running = customEntries.Where(r => r.Status == EntryStatus.Running);
+            var completedFailed = customEntries.Where(r => r.Status == EntryStatus.CompletedFailed);
+            var neverFinished = customEntries.Where(r => r.Status == EntryStatus.NeverFinished);
+
+            //"CompletedSuccess"
+            //var portalsWithWarnings = allLogs.Where(log => log.Log.Contains("Warning")).OrderBy(l => l.PortalId).ToList();
+            //var storageAccount = CloudStorageAccount.Parse(Secrets.StorageAccountConnectionString);
+            //var queueClient = storageAccount.CreateCloudQueueClient();
+            //var queue = queueClient.GetQueueReference(Secrets.QueueName);
+            //foreach (var log in portalsWithWarnings)
+            //{
+            //    queue.AddMessage(new CloudQueueMessage(log.PortalId.ToString()));
+            //}
         }
     }
 }
